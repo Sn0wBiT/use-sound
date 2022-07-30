2bfc062a83ce8170b8e0ca4a73c333088bd15b08
Leopold <me@thientd.dev>
2022-07-30 22:44:49 +0700

    Removed dist folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type PlayFunction = () => void;
-export declare type ReturnValue = [PlayFunction];
-interface TuseSoundProps {
-    src: string;
-    volume?: number;
-}
-export declare const useSound: (props: TuseSoundProps) => ReturnValue;
-export default useSound;
